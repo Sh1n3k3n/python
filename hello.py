7adbb4a1c7ff14596e584c3bf5645a52926116a1
ken <3297370485@qq.com>
2018-01-10 12:10:05 +0800

    add hello.py

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 –*-
+''' 
+ -------------------------------
+ @Author:    KEN
+ @File:      hello.py
+ @Date:      2018/1/10 12:07
+ @Version:   1.0
+ @Function:  
+ @Useage:    
+ -------------------------------
+ @Update log:
+ 
+ -------------------------------
+'''
+print("hello world")
