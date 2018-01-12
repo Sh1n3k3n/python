b94fbb6557cd2d1002c3cf33e3505a7bf2153e2b
ken <3297370485@qq.com>
2018-01-12 14:55:35 +0800

    更新了两种方法实现 3级菜单 v1方法传统但通用性强，判断错误输入 v2方法思路不一样，通过一个缓存list实现返回上级

--- day1/school_menu_v1.py
+++ day1/school_menu_v1.py
@@ -3,7 +3,7 @@
 ''' 
  -------------------------------
  @Author:    KEN
- @File:      school_menu.py
+ @File:      school_menu_v1.py
  @Date:      2018/1/12 10:54
  @Version:   1.0
  @Function:  多级菜单，可按年级>班级，显示该年级该班的学生姓名
@@ -14,24 +14,24 @@
  -------------------------------
 '''
 import os
-dict_school = { 'grade1':{
-                    'class1_1':['1_1_a','1_1_b','1_1_c']},
-                'grade2':{
-                    'class2_1':['2_1_a','2_1_b','2_1_c'],
-                    'class2_2':['2_2_a','2_2_b','2_2_c']},
-                'grade3':{
-                    'class3_1':['3_1_a','3_1_b','3_1_c'],
-                    'class3_2':['3_2_a','3_2_b','3_2_c'],
-                    'class3_3':['3_3_a','3_3_b','3_3_c']}}
+dict_school = { '1年级':{
+                    '1-1班':['小王','小明','小红']},
+                '2年级':{
+                    '2-1班':['马云','马化腾','李彦宏'],
+                    '2-2班':['aaa','bbb','ccc']},
+                '3年级':{
+                    '3-1班':['小张','小李','小魔'],
+                    '3-2班':['小于','小游','小菲'],
+                    '3-3班':['Alice','Bob','John']}}
 
 list_grade = [(num_grade,grade_name) for num_grade,grade_name in enumerate(dict_school)]
-list_grade.append((len(list_grade),'exit')) #添加exit选项
+list_grade.append((len(list_grade),'退出')) #添加exit选项
 break_flag = False
 while True:
     if break_flag == True:
         break
     print('-----------------')
-    print('欢迎使用学生查询系统')
+    print('欢迎使用学级学生信息系统')
     print('-----------------')
 
     for i in list_grade:
@@ -39,21 +39,21 @@ while True:
             print(j,end=' ')
         print('')
 
-    get_grade = input("Choose one grade: ")
+    get_grade = input("选择一个年级: ")
 
     if not get_grade.isdecimal():
-        print('Please input a number!')
+        print('请输入数字!')
     elif int(get_grade) >= len(list_grade):
-        print('Number is too bigger!')
+        print('数字太大!')
     elif int(get_grade) == len(list_grade)-1:
-        print('Bye!')
+        print('再见!')
         break
     else:
         choose_grade = list_grade[int(get_grade)][1]
 
         list_class = [(num_class,class_name) for num_class,class_name in enumerate(dict_school[choose_grade])]
-        list_class.append((len(list_class),'return to choose grade'))
-        list_class.append((len(list_class),'exit'))
+        list_class.append((len(list_class),'返回上一级'))
+        list_class.append((len(list_class),'退出'))
 
         while True:
             for i in list_class:
@@ -61,20 +61,20 @@ while True:
                     print(j,end=' ')
                 print('')
 
-            get_class= input('Choose one class: ')
+            get_class= input('选择一个班级: ')
 
             if not get_class.isdigit():
-                print('Please input a number!')
+                print('请输入数字!')
             elif int(get_class) >= len(list_class):
-                print('Number is too bigger!')
+                print('数字太大!')
             elif int(get_class) == len(list_class)-2:
                 break
             elif int(get_class) == len(list_class)-1:
                 break_flag = True
-                print('Bye!')
+                print('再见!')
                 break
             else:
                 choose_class = list_class[int(get_class)][1]
 
-                print('Students names are : %s' % dict_school[choose_grade][choose_class])
+                print('该班学生有 : %s' % dict_school[choose_grade][choose_class])
 print('-----------------')
